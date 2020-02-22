a5ebcd353beaf77caf9c27bc3b4a7adf17b63cc0
Shivang Agrawal <shivangagrawal4@gmail.com>
2020-02-22 17:01:49 +0530

    Create main.js

---
+++ public/scripts/main.js
@@ -0,0 +1,97 @@
+function myFunction1() {
+  const moreText = document.querySelector("#more1");
+  const btnText = document.querySelector("#myBtn1");
+
+  if (btnText.textContent === "Read more") {
+    btnText.textContent = "Read less";
+    moreText.style.display = "inline";
+  }
+  else {
+    btnText.textContent = "Read more";
+    moreText.style.display = "none";
+  }
+}
+
+function myFunction2() {
+  const moreText = document.querySelector("#more2");
+  const btnText = document.querySelector("#myBtn2");
+
+  if (btnText.textContent === "Read more") {
+    btnText.textContent = "Read less";
+    moreText.style.display = "inline";
+  }
+  else {
+    btnText.textContent = "Read more";
+    moreText.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  const moreText = document.querySelector("#more3");
+  const btnText = document.querySelector("#myBtn3");
+
+  if (btnText.textContent === "Read more") {
+    btnText.textContent = "Read less";
+    moreText.style.display = "inline";
+  }
+  else {
+    btnText.textContent = "Read more";
+    moreText.style.display = "none";
+  }
+}
+
+function myFunction4() {
+  const moreText = document.querySelector("#more4");
+  const btnText = document.querySelector("#myBtn4");
+
+  if (btnText.textContent === "Read more") {
+    btnText.textContent = "Read less";
+    moreText.style.display = "inline";
+  }
+  else {
+    btnText.textContent = "Read more";
+    moreText.style.display = "none";
+  }
+}
+
+function myFunction5() {
+  const moreText = document.querySelector("#more5");
+  const btnText = document.querySelector("#myBtn5");
+
+  if (btnText.textContent === "Read more") {
+    btnText.textContent = "Read less";
+    moreText.style.display = "inline";
+  }
+  else {
+    btnText.textContent = "Read more";
+    moreText.style.display = "none";
+  }
+}
+
+function myFunction6() {
+  const moreText = document.querySelector("#more6");
+  const btnText = document.querySelector("#myBtn6");
+
+  if (btnText.textContent === "Read more") {
+    btnText.textContent = "Read less";
+    moreText.style.display = "inline";
+  }
+  else {
+    btnText.textContent = "Read more";
+    moreText.style.display = "none";
+  }
+}
+
+function myFunction7() {
+  const moreText = document.querySelector("#more7");
+  const btnText = document.querySelector("#myBtn7");
+
+  if (btnText.textContent === "Read more") {
+    btnText.textContent = "Read less";
+    moreText.style.display = "inline";
+  }
+  else {
+    btnText.textContent = "Read more";
+    moreText.style.display = "none";
+  }
+}
